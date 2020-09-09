c495ee1ea61d5d8028778916ad970d6d39e75d77
rajyan <kitakita7617@gmail.com>
2020-09-09 20:51:32 +0900

    added index

--- webapp/sql/02_add_indexes.sql
+++ webapp/sql/02_add_indexes.sql
@@ -1,4 +1,5 @@
 alter table `train_timetable_master` add index idx_dtsd(`date`,`train_name`, `station`, `departure`);
 alter table `reservations` add index idx_dcn(`date`,`train_class`, `train_name`);
+alter table `reservations` add index idx_u(`user_id`);
 alter table `train_master` add index idx_dcn(`date`,`train_class`, `train_name`);
 alter table `seat_reservations` add index idx_r(`reservation_id`);
