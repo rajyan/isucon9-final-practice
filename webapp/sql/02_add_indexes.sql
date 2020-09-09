cf8eed35125be176b717a28dcf6d463467027df5
rajyan <kitakita7617@gmail.com>
2020-09-10 00:12:32 +0900

    added index to user

--- webapp/sql/02_add_indexes.sql
+++ webapp/sql/02_add_indexes.sql
@@ -3,3 +3,4 @@ alter table `reservations` add index idx_dcn(`date`,`train_class`, `train_name`)
 alter table `reservations` add index idx_u(`user_id`);
 alter table `train_master` add index idx_dcn(`date`,`train_class`, `train_name`);
 alter table `seat_reservations` add index idx_r(`reservation_id`);
+alter table `users` add index idx_r(`email`);
